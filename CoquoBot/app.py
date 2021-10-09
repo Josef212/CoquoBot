e6ceade3090678bbc2613327102b73d1d2908901
Josef212 <jcasanovasv@hotmail.com>
2021-10-10 00:18:49 +0200

    Creating menu and order for the app

--- CoquoBot/app.py
+++ CoquoBot/app.py
@@ -1,37 +1,63 @@
-import os
 import logging
 import credentials
 
+from order import Order
+from menu import Menu
+from command import Command
+
+from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters
 
 class App(Updater):
     def __init__(self, token: str, logger: logging.Logger):
         super().__init__(token=token, use_context=True)
         self.log = logger
-        self.info('# App init')
+        self.info('# App init -----------')
 
-    def set_up(self):
+        self.order = Order()
+        self.menu = Menu(self)
+
+        self.__create_commands()
+        self.__inited = False
+
+    def set_up(self) -> None:
         self.dispatcher.add_error_handler(lambda u, c: self.__error_handler(u, c))
         self.dispatcher.add_handler(MessageHandler(Filters.text, lambda u, c: self.__echo(u, c)))
 
-    def run(self):
+        for cmd in self.__cmds:
+            self.dispatcher.add_handler(cmd.get_command())
+        
+        self.__inited = True
+
+    def run(self) -> None:
         self.start_polling()
         self.idle()
 
-    def info(self, msg: str):
+    def add_command(self, cmd: Command) -> None:
+        self.__cmds.append(cmd)
+        if self.__inited:
+            self.dispatcher.add_handler(cmd.get_command())
+
+    def info(self, msg: str) -> None:
         self.log.info(msg)
     
-    def warn(self, msg: str):
+    def warn(self, msg: str) -> None:
         self.log.warning(msg)
 
-    def error(self, msg: str):
+    def error(self, msg: str) -> None:
         self.log.error(msg)
 
-    def __error_handler(self, update, ctx):
+    def __error_handler(self, update: Update, ctx) -> None:
         self.warn(f'Update {update.message.text} caused error:\n  {ctx.error}')
     
-    def __echo(self, update, ctx):
+    def __echo(self, update: Update, ctx):
         update.message.reply_text(update.message.text)
+    
+    def __create_commands(self) -> None:
+        # Probably with decorators I could get all commands instead of manually adding them
+        self.__cmds = [
+
+        ]
 
 
 def main():
