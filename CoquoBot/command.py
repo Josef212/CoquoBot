a9d228473fba9caa247699eaffcfd13bc1035431
Josef212 <jcasanovasv@hotmail.com>
2021-10-10 01:39:13 +0200

    Added helper functions to base command

--- CoquoBot/command.py
+++ CoquoBot/command.py
@@ -20,6 +20,21 @@ class Command(metaclass=abc.ABCMeta):
 
     def get_help(self):
         return f'{self.name}: {self.name}'
+    
+    def get_username_from_update(self, update) -> str:
+        return update.message.from_user.user_name
+
+    def get_user_lang_from_update(self, update) -> str:
+        return update.message.from_user.language_code
+    
+    def get_chat_id_from_update(self, update) -> int:
+        return update.message.chat.id
+    
+    def reply_message(self, update, msg: str) -> None:
+        update.message.reply_text(msg)
+    
+    def edit_message(self, query, msg: str) -> None:
+        query.message.edit_message_text(msg)
 
 # Command implementation example
 class TestCmd(Command):
