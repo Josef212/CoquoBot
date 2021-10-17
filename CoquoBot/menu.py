54e6823d13af5c3abe2457e98fbdf6be45c8e6f8
Josef212 <jcasanovasv@hotmail.com>
2021-10-17 16:43:49 +0200

    Updated menu

--- CoquoBot/menu.py
+++ CoquoBot/menu.py
@@ -1,22 +1,28 @@
 # TODO: Move this to a db
 BASE_MENU = {
     'FigaDeCabra': 2.50,
-    'BaconCheddar': 2.25,
+    'BaconCheddar': 2.50,
     '4Formatges': 2.50,
-    'Bbq': 2.25,
+    'Bbq': 2.50,
     'PolloCurry': 2.50,
     'Carabassó': 2.50,
     'Xampinyons': 2.25,
     'Llonganissa': 2.25,
     'Margarita': 2.25,
     'Pepperoni': 2.50,
-    'Nutella': 2.25,
+    'Nutella': 2.50,
     'Patata': 2.25,
     'FormatgesAmbPera': 2.50,
     'Botifarra': 2.50,
     'Carbonara': 2.50,
     'Alberginia': 2.50,
-    'Hawaiana': 2.50
+    'Hawaiana': 2.50,
+    #'Salsa-Olivada': 0.0,
+    #'Salsa-Pesto': 0.0,
+    #'Salsa-Bbq': 0.0,
+    #'Salsa-Anxoves': 0.0,
+    #'Salsa-MayonesaWasabi': 0.0,
+    #'Salsa-Brava': 0.0
 }
 
 URL = 'https://coquopizza.com/carta/'
